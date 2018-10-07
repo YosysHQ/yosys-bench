e2d01f1f6ce1a3dcbe730aa5971728ac6f56ba81
Niels Moseley <n.a.moseley@moseleyinstrumnets.com>
2018-10-07 18:39:23 +0200

    Added dual-port RAM and regular synchronous RAM to verilog dir

--- verilog/benchmarks_small/ram/generate.py
+++ verilog/benchmarks_small/ram/generate.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
-## Generate a synchronous RAM with 2^<w> addresses and <s> bit data path.
+## Generate various synchronous RAM types with 2^<w> addresses and <s> bit data path.
 
 import math
 
+# generic synchronous RAM
 def gen_syncram(addrWidth, ioWidth, template):
     with open("syncram_%d_%d.v" % (addrWidth, ioWidth), "w") as f:
         print(template.replace("<w>", str(addrWidth)).replace("<s>", str(ioWidth)), file=f)
+
+# generic synchronous RAM with transparent write-through
+def gen_syncram_tw(addrWidth, ioWidth, template):
+    with open("syncram_tw_%d_%d.v" % (addrWidth, ioWidth), "w") as f:
+        print(template.replace("<w>", str(addrWidth)).replace("<s>", str(ioWidth)), file=f)
+
+# generic single-write/single-read synchronous RAM
+def gen_dualportsyncram(addrWidth, ioWidth, template):
+    with open("dualport_syncram_%d_%d.v" % (addrWidth, ioWidth), "w") as f:
+        print(template.replace("<w>", str(addrWidth)).replace("<s>", str(ioWidth)), file=f)
+
+
 
 with open('syncram.template','rt') as templatefile:
     template = templatefile.read()
@@ -14,3 +27,17 @@ with open('syncram.template','rt') as templatefile:
     for ioWidth in [4,8,12,16]:
         for addrBits in [4,8,9,10,11,12]:
             gen_syncram(addrBits, ioWidth, ''.join(template))
+
+with open('syncram_tw.template','rt') as templatefile:
+    template = templatefile.read()
+    
+    for ioWidth in [4,8,12,16]:
+        for addrBits in [4,8,9,10,11,12]:
+            gen_syncram_tw(addrBits, ioWidth, ''.join(template))
+
+with open('dualport_syncram.template','rt') as templatefile:
+    template = templatefile.read()
+    
+    for ioWidth in [4,7,8,12,16]:
+        for addrBits in [4,8,9,10,11,12]:
+            gen_dualportsyncram(addrBits, ioWidth, ''.join(template))
