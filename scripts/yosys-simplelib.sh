62e8b6231612067d9c659d3aa054b7780069f2a6
Niels Moseley <n.a.moseley@moseleyinstrumnets.com>
2018-11-02 15:22:47 +0100

    Added 8-bit latch

--- scripts/yosys-simplelib.sh
+++ scripts/yosys-simplelib.sh
@@ -28,12 +28,13 @@ echo "hierarchy; proc; fsm; opt; memory; opt" >> script.yos
 echo "techmap; opt" >> script.yos
 echo "dfflibmap -liberty $celllibpath/simple/simple.lib" >> script.yos
 echo "abc -liberty $celllibpath/simple/simple.lib" >> script.yos
-#echo "write_verilog /$1_netlist.v" >> script.yos
-echo "stat --liberty"
+echo "write_verilog /$1_netlist.v" >> script.yos
+#echo "stat -liberty" >> script.yos
 #echo "strash; ifraig; scorr; dc2; dretime; strash; &get -n; &dch -f; &nf {D}; &put" > abc.script
 
 # run tools
-yosys -ql $logfile -p "script $scriptpath/script.yos" >/dev/null
+#yosys -ql $logfile -p "script $scriptpath/script.yos" >/dev/null
+yosys -l $logfile -p "script $scriptpath/script.yos" >/dev/null
 sed -r '/^[0-9\.]+ Printing statistics./,/^[0-9\.]+ / { /SB_LUT4/ { s/.* //; p; }; }; d;' $logfile
 cp $logfile $celllibpath/../log.txt
 rm -f $logfile
