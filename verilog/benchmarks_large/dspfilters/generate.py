d21cb5a1c72c032e5e2302b600182754ecd8deed
Eddie Hung <eddieh@ece.ubc.ca>
2019-03-20 16:58:19 -0700

    Add slowfil_fixedtaps.v

--- verilog/benchmarks_large/dspfilters/generate.py
+++ verilog/benchmarks_large/dspfilters/generate.py
@@ -2,4 +2,5 @@
 
 import urllib.request
 urllib.request.urlretrieve('https://raw.githubusercontent.com/ZipCPU/dspfilters/49b9a0235f88c34b9a997b1aa9a634ad130ea719/rtl/fastfir.v', 'fastfir.vh')
+urllib.request.urlretrieve('https://raw.githubusercontent.com/ZipCPU/dspfilters/49b9a0235f88c34b9a997b1aa9a634ad130ea719/rtl/slowfil.v', 'slowfil.vh')
 urllib.request.urlretrieve('https://raw.githubusercontent.com/ZipCPU/dspfilters/49b9a0235f88c34b9a997b1aa9a634ad130ea719/rtl/firtap.v', 'firtap.vh')
--- verilog/benchmarks_large/dspfilters/slowfil_fixedtaps.v
+++ verilog/benchmarks_large/dspfilters/slowfil_fixedtaps.v
@@ -1,4 +1,4 @@
-module  fastfir_fixedtaps(i_clk, i_reset, i_tap_wr, i_tap, i_ce, i_sample, o_result);
+module  slowfil_fixedtaps(i_clk, i_reset, i_tap_wr, i_tap, i_ce, i_sample, o_ce, o_result);
 `ifdef	FORMAL
 	parameter		NTAPS=16, IW=9, TW=IW, OW=2*IW+5;
 `else
@@ -12,10 +12,10 @@ module  fastfir_fixedtaps(i_clk, i_reset, i_tap_wr, i_tap, i_ce, i_sample, o_res
 	//
 	input	wire			i_ce;
 	input	wire	[(IW-1):0]	i_sample;
+	output	wire	          	o_ce;
 	output	wire	[(OW-1):0]	o_result;
 
-	fastfir #(.FIXED_TAPS(1), .NTAPS(NTAPS), .IW(IW)) fir (.i_clk(i_clk), .i_reset(i_reset), .i_tap_wr(i_tap_wr), .i_tap(i_tap), .i_ce(i_ce), .i_sample(i_sample), .o_result(o_result));
+	slowfil #(.FIXED_TAPS(1), .NTAPS(NTAPS), .IW(IW), .INITIAL_COEFFS("taps.hex")) fir (.i_clk(i_clk), .i_reset(i_reset), .i_tap_wr(i_tap_wr), .i_tap(i_tap), .i_ce(i_ce), .i_sample(i_sample), .o_ce(o_ce), .o_result(o_result));
 endmodule
 
-`include "fastfir.vh"
-`include "firtap.vh"
+`include "slowfil.vh"
