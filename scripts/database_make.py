56d983121244e602207e0974de30e5238f2b57fb
Eddie Hung <eddie@fpgeh.com>
2019-04-25 08:52:10 -0700

    Rework scripts/database_make.py to not enter git repos

--- scripts/database_make.py
+++ scripts/database_make.py
@@ -51,53 +51,59 @@ for dir in sys.argv[2:]:
 
 # execute all .v or .vhdl scripts specified in the config.json file
 # or if there is no config.json, simply walk the directory.
-for dir in sys.argv[2:]:
-    print("Processing directory: " + dir)
-    for subdir, dirs, files in os.walk(dir):
-        # check if there is a config.json file
-        configFileName = subdir + "/config.json"
-        if (os.path.isfile(configFileName)):
-            print("  Running config file:" + configFileName)
-            with open(configFileName, 'r') as configFile:
-                try:
-                    config = json.load(configFile)
-                    executeConfig(celllibpath, shellScriptName, dbpath, subdir, config)
-                except ValueError as error:
-                    print("  --- ERROR PARSING CONFIG.JSON ---")
-                    pass
-        else:
-            for file in files:
-                if (file.endswith(".v")):
-                    # skip all files that end in _tb.v as they are testbench files
-                    # containing unsynthesizable code
-                    if (file.endswith("_tb.v")):
-                        print("  Skipping Verilog testbench file " + file)
-                        continue
-                    # skip any netlist files that might have been produced in 
-                    # previous runs
-                    if (file.endswith("_netlist.v")):
-                        print("  Skipping Verilog netlist file " + file)
-                        continue
-                    verilogsrc = os.path.join(subdir, file)
-                    filewithoutext, file_extension = os.path.splitext(file)
-                    datfile = open(os.path.join(dbpath, filewithoutext + ".dat"), "wt")
-                    print("  Running Verilog file " + file)
-                    retval = subprocess.check_call([os.path.abspath("./scripts/"+shellScriptName+".sh"), os.path.abspath("./" +verilogsrc), celllibpath],
-                                                cwd=os.path.abspath(subdir),
-                                                stdout=datfile,
-                                                stderr=sys.stderr
-                                                )
-                    datfile.close()
+dir = sys.argv[2]
+queue = [ dir ]
+print("Processing directory: " + dir)
+while queue:
+    subdir = queue.pop()
+    listdir = os.listdir(subdir)
+    # Do not enter git repositories
+    if '.git' in listdir: continue
+    for item in listdir:
+        path = os.path.join(subdir, item)
+        if os.path.isdir(path):
+            queue.append(path)
+        elif os.path.isfile(path):
+            # check if there is a config.json file
+            if item == 'config.json':
+                print("  Running config file: " + item)
+                with open(path, 'r') as configFile:
+                    try:
+                        config = json.load(configFile)
+                        executeConfig(celllibpath, shellScriptName, dbpath, subdir, config)
+                    except ValueError as error:
+                        print("  --- ERROR PARSING CONFIG.JSON ---")
+                        pass
+            if (item.endswith(".v")):
+                # skip all files that end in _tb.v as they are testbench files
+                # containing unsynthesizable code
+                if (item.endswith("_tb.v")):
+                    print("  Skipping Verilog testbench file " + item)
+                    continue
+                # skip any netlist files that might have been produced in 
+                # previous runs
+                if (item.endswith("_netlist.v")):
+                    print("  Skipping Verilog netlist file " + item)
+                    continue
+                verilogsrc = os.path.join(subdir, item)
+                filewithoutext, file_extension = os.path.splitext(item)
+                datfile = open(os.path.join(dbpath, filewithoutext + ".dat"), "wt")
+                print("  Running Verilog file " + item)
+                retval = subprocess.check_call([os.path.abspath("./scripts/"+shellScriptName+".sh"), os.path.abspath("./" +verilogsrc), celllibpath],
+                                            cwd=os.path.abspath(subdir),
+                                            stdout=datfile,
+                                            stderr=sys.stderr
+                                            )
+                datfile.close()
                     
-            for file in files:
-                if (file.endswith(".vhdl")):
-                    vhdlsrc = os.path.join(subdir, file)
-                    filewithoutext, file_extension = os.path.splitext(file)
-                    datfile = open(os.path.join(dbpath, filewithoutext + ".dat"), "wt")
-                    print("  Running VHDL file " + file)
-                    retval = subprocess.check_call([os.path.abspath("./scripts/"+shellScriptName+".sh"),os.path.abspath("./" +vhdlsrc), celllibpath],
-                                                cwd=os.path.abspath(subdir),
-                                                stdout=datfile,
-                                                stderr=sys.stderr
-                                                )
-                    datfile.close()                
+            if (item.endswith(".vhdl")):
+                vhdlsrc = os.path.join(subdir, item)
+                filewithoutext, file_extension = os.path.splitext(item)
+                datfile = open(os.path.join(dbpath, filewithoutext + ".dat"), "wt")
+                print("  Running VHDL file " + item)
+                retval = subprocess.check_call([os.path.abspath("./scripts/"+shellScriptName+".sh"),os.path.abspath("./" +vhdlsrc), celllibpath],
+                                            cwd=os.path.abspath(subdir),
+                                            stdout=datfile,
+                                            stderr=sys.stderr
+                                            )
+                datfile.close()
