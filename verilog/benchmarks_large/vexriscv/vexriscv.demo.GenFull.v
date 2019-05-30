40dc80f4b117a76c0ab9456727726215fe611c69
Eddie Hung <eddie@fpgeh.com>
2019-05-30 16:18:34 -0700

    Escape name

--- verilog/benchmarks_large/vexriscv/vexriscv.demo.GenFull.v
+++ verilog/benchmarks_large/vexriscv/vexriscv.demo.GenFull.v
@@ -1056,7 +1056,7 @@ module DataCache (
 
 endmodule
 
-module vexriscv.demo.GenFull (
+module \vexriscv.demo.GenFull (
       input   timerInterrupt,
       input   externalInterrupt,
       input   softwareInterrupt,
