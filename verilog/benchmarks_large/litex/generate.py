cb6761c933f76ff62a2f5fe6ba2a6cbafc568329
Eddie Hung <eddie@fpgeh.com>
2019-06-19 11:18:21 -0700

    Instantiate BUFGCE inside our own EICG_wrapper

--- verilog/benchmarks_large/litex/generate.py
+++ verilog/benchmarks_large/litex/generate.py
@@ -2,5 +2,4 @@
 
 import subprocess
 import urllib.request
-subprocess.run(['touch', 'mem.init', 'mem_1.init'])
 urllib.request.urlretrieve('https://raw.githubusercontent.com/enjoy-digital/litex/33d7cc5fc81cc7785be217f64d756db6092aeff6/litex/soc/cores/cpu/lm32/verilog/config/lm32_config.v', 'lm32/rtl/lm32_config.v')
