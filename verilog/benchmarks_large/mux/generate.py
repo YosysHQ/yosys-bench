d07cc918b535713757a82d2d88e96729201e39bd
Eddie Hung <eddie@fpgeh.com>
2019-06-26 11:52:54 -0700

    Add boundary+2 to mux tests

--- verilog/benchmarks_large/mux/generate.py
+++ verilog/benchmarks_large/mux/generate.py
@@ -3,7 +3,7 @@
 from common import *
 
 if __name__ == "__main__":
-    for N in [63,64,65] + [127,128,129] + [255,256,257]:
+    for N in [63,64,65,66] + [127,128,129,130] + [255,256,257,258]:
         for W in [8,16,32]:
             gen_mux_index(N,W)
             gen_mux_case(N,W)
--- verilog/benchmarks_small/mux/generate.py
+++ verilog/benchmarks_small/mux/generate.py
@@ -3,7 +3,7 @@
 from common import *
 
 if __name__ == "__main__":
-    for N in [2,3,4,5] + [7,8,9] + [15,16,17] + [31,32,33]:
+    for N in [2,3,4,5,6] + [7,8,9,10] + [15,16,17,18] + [31,32,33,34]:
         for W in [1,2,3,4,5,8]:
             gen_mux_index(N,W)
             gen_mux_case(N,W)
