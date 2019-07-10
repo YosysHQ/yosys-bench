b715dafdb9e457fb4de76b99a6dcd26f4b043c07
Eddie Hung <eddie@fpgeh.com>
2019-07-10 22:36:12 +0000

    Add *_vivado.tcl files for litex benchmarks

---
+++ verilog/benchmarks_large/litex/soc_ethernetsoc_arty_rocket_vivado.tcl
@@ -0,0 +1,8 @@
+read_verilog soc_ethernetsoc_arty_rocket.v
+read_verilog rocket-litex-verilog/vsrc/plusarg_reader.v
+read_verilog rocket-litex-verilog/generated-src/freechips.rocketchip.system.LitexConfig.v
+#read_verilog rocket-litex-verilog/vsrc/EICG_wrapper.v
+read_verilog EICG_wrapper.v
+read_verilog rocket-litex-verilog/generated-src/freechips.rocketchip.system.LitexConfig.behav_srams.v
+read_verilog rocket-litex-verilog/vsrc/AsyncResetReg.v
+set_property top TOP [current_fileset]
---
+++ verilog/benchmarks_large/litex/soc_ethernetsoc_arty_vexriscv_vivado.tcl
@@ -0,0 +1,2 @@
+read_verilog soc_ethernetsoc_arty_vexriscv.v
+read_verilog VexRiscv-verilog/VexRiscv.v
