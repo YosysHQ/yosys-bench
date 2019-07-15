c5222aaac3b4be31047a7843884ceadb5cd0dee8
Eddie Hung <eddie@fpgeh.com>
2019-07-15 11:33:24 -0700

    Add basic multiplier benchmark

--- verilog/benchmarks_small/mul/generate.py
+++ verilog/benchmarks_small/mul/generate.py
@@ -8,17 +8,14 @@ for A in ARange:
         with open("mul_%d_%d.v" % (A,B), "w") as f:
             print("""
 (* top *)
-module mul_{0}_{1} #(parameter A={0}, parameter B={1}) (input [A-1:0] i, input [B-1:0] s, output [A+B-1:0] o);
+module mul_{0}_{1} #(parameter AW={0}, parameter BW={1}) (input [AW-1:0] A, input [BW-1:0] B, output [AW+BW-1:0] o);
 assign o = A * B;
 endmodule
 """.format(A,B), file=f)
-        print(A,B)
-        B,A = A,B
-        print(A,B)
-        with open("mul_%d_%d.v" % (A,B), "w") as f:
+        with open("mul_%d_%d.v" % (B,A), "w") as f:
             print("""
 (* top *)
-module mul_{0}_{1} #(parameter A={0}, parameter B={1}) (input [A-1:0] i, input [B-1:0] s, output [A+B-1:0] o);
+module mul_{0}_{1} #(parameter AW={0}, parameter BW={1}) (input [AW-1:0] A, input [BW-1:0] B, output [AW+BW-1:0] o);
 assign o = A * B;
 endmodule
-""".format(A,B), file=f)
+""".format(B,A), file=f)
