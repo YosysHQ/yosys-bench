36353b609df1aee271650bd674e2bae1e393b703
Eddie Hung <eddie@fpgeh.com>
2019-07-16 14:15:33 -0700

    Add enable support

--- verilog/benchmarks_small/mul/generate.py
+++ verilog/benchmarks_small/mul/generate.py
@@ -2,6 +2,28 @@
 
 ARange = [4,8,16,24,25]
 BRange = [2,4,8,16,18]
+rtl = """(* top *)
+module mul_{0}_{1}_{2}_{3} #(parameter AW={0}, BW={1}, AREG={4}, BREG={5}, PREG={6}) (input clk, CEA, CEB, CEP, input [AW-1:0] A, input [BW-1:0] B, output reg [AW+BW-1:0] P);
+reg [AW-1:0] Ar;
+reg [BW-1:0] Br;
+generate
+    if (AREG) begin
+        always @(posedge clk) if ({7}) Ar <= A;
+    end
+    else
+        always @* Ar <= A;
+    if (BREG) begin
+        always @(posedge clk) if ({8}) Br <= B;
+    end
+    else
+        always @* Br <= B;
+    if (PREG) begin
+        always @(posedge clk) if ({9}) P <= Ar * Br;
+    end
+    else
+        always @* P <= Ar * Br;
+endgenerate
+endmodule"""
 
 # https://stackoverflow.com/a/1482316
 from itertools import chain, combinations
@@ -12,46 +34,12 @@ def powerset(iterable):
 
 for A in ARange:
     for B in BRange:
-        for R in map(lambda i:''.join(i), powerset("ABP")):
-            with open("mul_%d_%d_%s.v" % (A,B,R), "w") as f:
-                print("""
-(* top *)
-module mul_{0}_{1}_{2} #(parameter AW={0}, parameter BW={1}, AREG={3}, BREG={4}, OREG={5}) (input clk, input [AW-1:0] A, input [BW-1:0] B, output reg [AW+BW-1:0] o);
-reg [AW-1:0] Ar;
-reg [BW-1:0] Br;
-generate
-    if (AREG)
-        always @(posedge clk) Ar <= A;
-    else
-        always @* Ar <= A;
-    if (BREG)
-        always @(posedge clk) Br <= B;
-    else
-        always @* Br <= B;
-    if (OREG)
-        always @(posedge clk) o <= A *B;
-    else
-        always @* o <= A * B;
-endgenerate
-endmodule""".format(A,B,R,'1' if 'A' in R else '0', '1' if 'B' in R else '0', '1' if 'O' in R else '0'), file=f)
-            with open("mul_%d_%d_%s.v" % (B,A,R), "w") as f:
-                print("""
-(* top *)
-module mul_{0}_{1}_{2} #(parameter AW={0}, parameter BW={1}, AREG={3}, BREG={4}, OREG={5}) (input clk, input [AW-1:0] A, input [BW-1:0] B, output reg [AW+BW-1:0] o);
-reg [AW-1:0] Ar;
-reg [BW-1:0] Br;
-generate
-    if (AREG)
-        always @(posedge clk) Ar <= A;
-    else
-        always @* Ar <= A;
-    if (BREG)
-        always @(posedge clk) Br <= B;
-    else
-        always @* Br <= B;
-    if (OREG)
-        always @(posedge clk) o <= A *B;
-    else
-        always @* o <= A * B;
-endgenerate
-endmodule""".format(B,A,R,'1' if 'B' in R else '0', '1' if 'A' in R else '0', '1' if 'O' in R else '0'), file=f)
+        for R in map(lambda i:''.join(i), powerset("ABP")): # Register existence
+            for E in map(lambda i:''.join(i), powerset(R)): # Enable
+                with open("mul_%d_%d_%s_%s.v" % (A,B,R,E), "w") as f:
+                    print(rtl.format(A,B,R,E,'1' if 'A' in R else '0', '1' if 'B' in R else '0', '1' if 'P' in R else '0',
+                        'CEA' if 'A' in E else '1', 'CEB' if 'B' in E else '1', 'CEP' if 'P' in E else '1'), file=f)
+                if A != B:
+                   with open("mul_%d_%d_%s_%s.v" % (B,A,R,E), "w") as f:
+                        print(rtl.format(B,A,R,E,'1' if 'B' in R else '0', '1' if 'A' in R else '0', '1' if 'P' in R else '0',
+                            'CEA' if 'B' in E else '1', 'CEB' if 'A' in E else '1', 'CEP' if 'P' in E else '1'), file=f)
