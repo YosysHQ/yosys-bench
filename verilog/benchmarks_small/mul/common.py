60245d9a2f08c6082a6fb76e051cd8bcfb5ce39f
Eddie Hung <eddie@fpgeh.com>
2019-09-03 13:06:55 -0700

    Add option for M register

--- verilog/benchmarks_small/mul/common.py
+++ verilog/benchmarks_small/mul/common.py
@@ -1,31 +1,37 @@
 
 
-def rtl_mul(name, AW, BW, AREG, BREG, PREG, Asigned, Bsigned, CEA, CEB, CEP):
+def rtl_mul(name, AW, BW, AREG, BREG, MREG, PREG, Asigned, Bsigned, CEA, CEB, CEM, CEP):
     return """(* top *)
-module {0} #(parameter AW={1}, BW={2}, AREG={3}, BREG={4}, PREG={5}) (input clk, CEA, CEB, CEP, input {6}[AW-1:0] A, input {7}[BW-1:0] B, output reg {8}[AW+BW-1:0] P);
-reg {6}[AW-1:0] Ar;
-reg {7}[BW-1:0] Br;
+module {0} #(parameter AW={1}, BW={2}, AREG={3}, BREG={4}, MREG={5}, PREG={6}) (input clk, CEA, CEB, CEM, CEP, input {7}[AW-1:0] A, input {8}[BW-1:0] B, output reg {9}[AW+BW-1:0] P);
+reg {7}[AW-1:0] Ar;
+reg {8}[BW-1:0] Br;
+reg {9}[AW+BW-1:0] Mr;
 generate
     if (AREG) begin
-        always @(posedge clk) if ({9}) Ar <= A;
+        always @(posedge clk) if ({10}) Ar <= A;
     end
     else
         always @* Ar <= A;
     if (BREG) begin
-        always @(posedge clk) if ({10}) Br <= B;
+        always @(posedge clk) if ({11}) Br <= B;
     end
     else
         always @* Br <= B;
-    if (PREG) begin
-        always @(posedge clk) if ({11}) P <= Ar * Br;
+    if (MREG) begin
+        always @(posedge clk) if ({12}) Mr <= Ar * Br;
     end
     else
-        always @* P <= Ar * Br;
+        always @* Mr <= Ar * Br;
+    if (PREG) begin
+        always @(posedge clk) if ({13}) P <= Mr;
+    end
+    else
+        always @* P <= Mr;
 endgenerate
 endmodule""".format(name, AW, BW, 
-                    '1' if AREG else '0', '1' if BREG else '0', '1' if PREG else '0',
+                    '1' if AREG else '0', '1' if BREG else '0', '1' if MREG else '0', '1' if PREG else '0',
                     'signed ' if Asigned else '', 'signed ' if Bsigned else '', 'signed ' if Asigned and Bsigned else '',
-                    'CEA' if CEA else '1', 'CEB' if CEB else '1', 'CEP' if CEP else '1')
+                    'CEA' if CEA else '1', 'CEB' if CEB else '1', 'CEM' if CEM else '1', 'CEP' if CEP else '1')
 
 # https://stackoverflow.com/a/1482316
 from itertools import chain, combinations
@@ -40,17 +46,17 @@ def gen_mul(aRange, bRange, reg="ABP"):
             for R in map(lambda i:''.join(i), powerset(reg)): # Register existence
                 for E in map(lambda i:''.join(i), powerset("")): # Enable
                     with open("mul_%s_%s_%s_%s.v" % (A,B,R,E), "w") as f:
-                        print(rtl_mul('mul_%s_%s_%s_%s' % (A,B,R,E),    # name
-                                      A.rstrip('s'), B.rstrip('s'),     # [AB]W
-                                      'A' in R, 'B' in R, 'P' in R,     # [ABP]REG
-                                      's' in A, 's' in B,               # [AB]_signed
-                                      'A' in E, 'B' in E, 'P' in E,     # CE[ABP]
+                        print(rtl_mul('mul_%s_%s_%s_%s' % (A,B,R,E),            # name
+                                      A.rstrip('s'), B.rstrip('s'),             # [AB]W
+                                      'A' in R, 'B' in R, 'M' in R, 'P' in R,   # [ABMP]REG
+                                      's' in A, 's' in B,                       # [AB]_signed
+                                      'A' in E, 'B' in E, 'M' in E, 'P' in E,   # CE[ABMP]
                                       ), file=f)
                     if A != B:
                         with open("mul_%s_%s_%s_%s.v" % (B,A,R,E), "w") as f:
-                            print(rtl_mul('mul_%s_%s_%s_%s' % (B,A,R,E),    # name
-                                          B.rstrip('s'), A.rstrip('s'),     # [AB]W
-                                          'B' in R, 'A' in R, 'P' in R,     # [ABP]REG
-                                          's' in B, 's' in A,               # [AB]_signed
-                                          'B' in E, 'A' in E, 'P' in E,     # CE[ABP]
+                            print(rtl_mul('mul_%s_%s_%s_%s' % (B,A,R,E),            # name
+                                          B.rstrip('s'), A.rstrip('s'),             # [AB]W
+                                          'B' in R, 'A' in R, 'M' in R, 'P' in R,   # [ABMP]REG
+                                          's' in B, 's' in A,                       # [AB]_signed
+                                          'B' in E, 'A' in E, 'M' in E, 'P' in E,   # CE[ABP]
                                           ), file=f)
