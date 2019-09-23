4afa0c51271cbc7a7f1a56c4e391da8a877c4ed8
Eddie Hung <eddie@fpgeh.com>
2019-09-23 10:42:02 -0700

    Fix macc/generate.py, reduce scope of all generates

--- verilog/benchmarks_small/muladd/generate.py
+++ verilog/benchmarks_small/muladd/generate.py
@@ -2,9 +2,9 @@
 
 from common import gen_muladd
 
-ARange = ['16','16s','24','24s','32','32s']
-BRange = ['2','2s','4','4s','8','8s','16','16s']
-CRange = ['16','16s','24','24s','32','32s']
+ARange = ['16','32s']
+BRange = ['8','16s']
+CRange = ['32','40s']
 
 if __name__ == "__main__":
     gen_muladd(ARange, BRange, CRange)
